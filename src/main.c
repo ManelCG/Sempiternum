8116524b8b88824319df58e4ede58b96ed3a3b40
MorktHrad <manelcg@protonmail.com>
2022-03-09 23:53:35 +0300

    Adjusted function calls for bug fix

--- src/main.c
+++ src/main.c
@@ -23,11 +23,11 @@ int main(int argc, char *argv[]){
   start_g = clock();
   printf("Started whole program clock\n");
 
-  int N = 100000;   //Max iterations
-  int h = 1000;
-  int w = 1000;
+  int N = 1000000;   //Max iterations
+  int h = 10000;
+  int w = 10000;
   int A = h*w;
-  double c[2] = {0, 1};   //If parameter space this is interpreted as Z
+  double c[2] = {0.26, 0};   //If parameter space this is interpreted as Z
   double Sx[2] = {-2, 2};
   double Sy[2] = {-2, 2};
 
@@ -36,13 +36,14 @@ int main(int argc, char *argv[]){
 
   printf("Drawing julia by backwards iteration...\n");
   start = clock();
-  unsigned char *m = draw_julia_backwards_iteration(N, h, w, c, 100000000);
+  unsigned char *m = draw_julia_backwards_iteration(N, h, w, c, 100000);
   end = clock();
-  printf("Donw. Took %f seconds\n", ((double)(end - start))/CLOCKS_PER_SEC);
+  printf("Done. Took %f seconds\n", ((double)(end - start))/CLOCKS_PER_SEC);
   lodepng_encode_file("test.png", m, w, h, LCT_GREY, 8);
 
   ////rec_f or parameter_space
-  //draw_julia_zoom(10000, N, h, w, c, p, "rec_f");
+  //draw_julia_zoom(0, N, h, w, c, p, "rec_f");
   //end_g = clock();
   //printf("Finished whole program execution. Took %f seconds\n", ((double)(end_g - start_g))/CLOCKS_PER_SEC);
+
 }
